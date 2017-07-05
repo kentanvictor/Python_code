27af0fdf232a5082f9c0ac23f4a49b3f64ded12c
kentanvictor <2030457926@qq.com>
2017-07-05 23:25:39 +0800

    實戰

--- baike_spider/spider_main.py
+++ baike_spider/spider_main.py
@@ -8,9 +8,14 @@ class SpiderMain(object):
         self.downloader = html_downloader.HtmlDownLoader()
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
-    def craw(self, root_url):
 
-        pass
+    def craw(self, root_url):
+        self.urls.add_new_url(root_url)
+        while self.urls.has_new_url():
+            new_url = self.urls.get_new_url()
+            html_cont = self.downloader.download(new_url)
+            new_urls, new_data = self.parser.parse(new_url, html_cont)
+            self.urls.add_new_urls(new_urls)
 
 
 if __name__ == "_main_":
